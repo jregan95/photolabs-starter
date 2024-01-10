19840f666d4c236680108f8e0283a8e0652d2c56
Jill Regan <jillian.jnr@hotmail.com>
2024-01-10 17:43:48 -0500

    Fix: Updated to include less tags

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,15 +6,15 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
   return (
-    <div className ="photo-list_item">
-      <figure >
+    <div className ="photo-list__item">
+
         <img className="photo-list__image" src={props.photo.imageSource}></img>
         <img className="photo-list__user-profile"src={props.photo.profile}></img>
-      </figure>
-      <figcaption className="photo-list__user-details">
+
+      <div  className="photo-list__user-details">
         <p className="photo-list__user-info">{props.photo.username}</p>
         <p className="photo-list__user-location">{props.photo.location.city} {props.photo.location.country}</p>
-      </figcaption>
+      </div>
     </div>
   )
 };
