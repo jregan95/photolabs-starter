bfb228ccee940a6def1a88d5c17c874f9b9cbb23
Jill Regan <jillian.jnr@hotmail.com>
2024-01-10 17:40:15 -0500

    Add: class names to photoListItem to add CSS styling

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,11 +6,15 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
   return (
-    <div>
-      <img src={props.photo.imageSource}></img>
-      <img src={props.photo.profile}></img>
-      <p>{props.photo.username}</p>
-      <p>{props.photo.location.city} {props.photo.location.country}</p>
+    <div className ="photo-list_item">
+      <figure >
+        <img className="photo-list__image" src={props.photo.imageSource}></img>
+        <img className="photo-list__user-profile"src={props.photo.profile}></img>
+      </figure>
+      <figcaption className="photo-list__user-details">
+        <p className="photo-list__user-info">{props.photo.username}</p>
+        <p className="photo-list__user-location">{props.photo.location.city} {props.photo.location.country}</p>
+      </figcaption>
     </div>
   )
 };
