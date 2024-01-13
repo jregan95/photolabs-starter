10011ef4493a4b90cf65446a0c3cf382148d0d23
Jill Regan <jillian.jnr@hotmail.com>
2024-01-13 14:34:23 -0500

    Update: create the template for actions for reducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,45 +1,53 @@
-import { useReducer, useState } from 'react';
+import { useState } from 'react';
 
-const ACTIONS = {
+export const ACTIONS = {
   FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
   FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
   SET_PHOTO_DATA: 'SET_PHOTO_DATA',
   SET_TOPIC_DATA: 'SET_TOPIC_DATA',
   SELECT_PHOTO: 'SELECT_PHOTO',
   DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
-};
+}
+
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.FAV_PHOTO_ADDED:
+      return {...state, favourites: [...state.favourites, action.payload]}
+    { /* insert all relevant actions as case statements*/ }  
+    
+
+    case ACTIONS.FAV_PHOTO_REMOVED:
+      return {...state, favourites: state.favourites.filter((id) => id !== action.payload)
+      }
+
+    case ACTIONS.SELECT_PHOTO:
+      return {...state, photoInfo: action.payload}
+
+    case ACTIONS.DISPLAY_PHOTO_DETAILS:
+      return{...state, photoInfo}
+
+    case ACTIONS.SET_PHOTO_DATA:
+      return {...state, photoData: action.payload}
+
+    case ACTIONS.SET_TOPIC_DATA:
+      return {...state, topicData: action.payload}
+      
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
+
 
 const useApplicationData = () => {
 
-  const [state, setState] = useReducer(reducer, {
+  const [state, setState] = useState({
     openModal: false,
     favourites: [],
     photoInfo: null,
   });
-
-
-  function reducer(state, action) {
-    switch (action.type) {
-      case ACTIONS.FAV_PHOTO_ADDED:
-        const newFavourites = [...state.favourites, action.payload];
-        return {...state, favourites: newFavourites }
-
-        case ACTIONS.FAV_PHOTO_REMOVED:
-          const updatedFavourites = state.favourites.filter((fav) => fav !== action.payload);
-          return { ...state, favourites: updatedFavourites };
-
-          case ACTIONS.SET_PHOTO_INFO:
-            return { ...state, photoInfo: action.payload };
-      
-          case ACTIONS.OPEN_MODAL:
-            return { ...state, openModal: !state.openModal };
-      
-      default:
-        throw new Error(
-          `Tried to reduce with unsupported action type: ${action.type}`
-        );
-    }
-  }
 
 
   const openModal = () => {
@@ -71,27 +79,15 @@ const useApplicationData = () => {
     return state.favourites && state.favourites.length > 0;
     }
 
-
-  return {
-    state,
-    openModal,
-    setPhotoInfo,
-    favouritesData,
-    ifFavouritesExist
-  }
-}
-
-
-
-
-    
-
-
-
-
-
+    return {
+      state,
+      openModal,
+      setPhotoInfo,
+      favouritesData,
+      ifFavouritesExist
+    }
   
-
+};
 
 export default useApplicationData;
 
